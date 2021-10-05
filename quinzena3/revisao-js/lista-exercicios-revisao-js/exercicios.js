3a8540a50c61dd81edf14405d9516af8ad262d6c
Luiz Fabio Szheer <you@example.com>
2021-10-04 23:21:14 -0300

    Exercicio 4

---
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -0,0 +1,146 @@
+// EXERCÍCIO 01
+
+
+function inverteArray(array) {
+ 
+  
+}
+
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+
+}
+
+// EXERCÍCIO 03
+const soQueroOSpares = [1, 2, 3, 4, 5, 6]
+function retornaNumerosPares(array) {
+ 
+
+  
+ 
+} 
+
+
+// EXERCÍCIO 04
+const maiorNumeroDoArray = [1, 2, 3, 4, 5, 6]
+
+function retornaMaiorNumero(array) {
+  return Math.max(...array)
+  }
+
+console.log(maiorNumeroDoArray)
+
+// EXERCÍCIO 05
+
+const tamanhoDoArray = [1, 2, 3, 4, 5, 6]
+function retornaQuantidadeElementos(array) {
+  return array.length
+}
+console.log(retornaQuantidadeElementos(tamanhoDoArray))
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+
+}
+
+// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+
+}
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+}
+
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+}
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
+}
+
+// EXERCÍCIO 12
+function filmeFavorito() {
+
+}
+
+// EXERCÍCIO 13
+function imprimeChamada() {
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+}
+
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+
+}
+
+// EXERCÍCIO 15
+function anonimizaPessoa(pessoa) {
+
+}
+
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
+
+}
